85169f8f87a60973c44b63eb6aebd1fc3f1bbb96
Ale <alexra1205@gmail.com>
2021-05-29 11:29:52 +0200

    reescrito script ICP a ver si es algo más claro

--- scripts/icp_node.py
+++ scripts/icp_node.py
@@ -10,33 +10,43 @@ import copy
 
 class Loc:
     def __init__(self):
-        #self.pose_ini = True
-        self.punto = np.asarray([0.0,0.0,0.0,1.0])
+        rate = rospy.Rate(10)
+        self.initial_pose = np.asarray([0.0,0.0,0.0,1.0]) # punto inicial ()
         self.icp_init = False # Bandera para ICP en instante inicial
         self.T_init=np.asarray([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]) # asumimos que no se mueve inicialmente
-        self.new_point=np.zeros((4,1))
+        self.punto=np.zeros((4,1))
 
+        # topic con la pose para hallar la inicial
         pose_subs = rospy.Subscriber("/ual/pose", PoseStamped, self.subscriber_callback)
 
+        # cuando recibimos la posicion, continuamos
+        while self.initial_pose[0] == 0.0:
+            rate.sleep()
+
+        # no necesitamos la pose inicial mas por lo que nos desuscribimos
+        pose_subs.unregister()
+
+        # publicador donde pondremos la posicion obtenida
         self.pos_T_pub = rospy.Publisher('/del_uav/ICP_position', Pose, queue_size=10)
 
-        rate = rospy.Rate(10)
+        # nos suscribimos al topic del lidar
         print("[ICP] Waiting for Lidar topic...")
         laser_sub = rospy.Subscriber('/del_uav/uav_laser_scan', PointCloud2, self.sub_callback)
         print("[ICP] Lidar topic detected. Waiting for first PointCloud capture.")
+
+        # tengo que hacer una captura inicial
         # La captura se hace en el callback
         while self.icp_init is False:
             rate.sleep()
+        
+        # guardo nube y punto como la de los instantes anteriores
         self.nube_ant = self.nube
-        #self.nube_ant = o3d.io.read_point_cloud("test_point_cloud_old.pcd")
+        self.punto_ant = self.initial_pose
         print("[ICP] Fin del init")
 
     def subscriber_callback(self, data):
-        #if self.pose_ini == True:
-        self.pose_real = np.asarray([data.pose.position.x, data.pose.position.y, data.pose.position.z, 1])
+        self.initial_pose = np.asarray([data.pose.position.x, data.pose.position.y, data.pose.position.z, 1])
         self.orientation = np.asarray([data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.z])
-
-        #self.pose_ini == False
 
         #self.punto[0] = round(self.punto[0],3)
         #self.punto[1] = round(self.punto[1],3)
@@ -48,28 +58,32 @@ class Loc:
 
     def icp_process(self):
         # Parto de la nube en el instante actual (self.nube) y la nube en el instante anterior (self.nube_ant)
-        #print(self.nube)
-        #print(self.nube_ant)
 
         # --Inserta procedimientos de filtrado aqui--
         threshold = 0.02
         # ICP punto a punto (opcion por defecto) / Partimos de que el robot no se mueve de una posicion a la siguiente (opcion por defecto)
-        self.T = o3d.registration.registration_icp(self.nube_ant, self.nube, threshold)
+        self.T = o3d.registration.registration_icp(self.nube_ant, self.nube, threshold, self.T_init, o3d.registration.TransformationEstimationPointToPoint())
+        self.T_init = self.T.transformation
 
+        # esto imprime info de la convergencia
         print(self.T)
         print("Transformation is:")
-        print(self.T.transformation)
+        print(self.T.transformation) # la matriz de transformacion
         print("")
         #self.draw_registration_result()
 
-        # calculo de punto
-        test = np.transpose(self.punto)
-        self.new_point = np.matmul(self.T.transformation, test)
-        print(self.new_point)
-        #print(self.punto)
-        print(self.pose_real)
+        # calculo de punto, es T por el punto anterior, me da el nuevo
+        self.punto = np.matmul(self.T.transformation, self.punto_ant)
 
-        tolist = self.new_point.tolist()
+
+        #test = np.transpose(self.punto)
+        #self.new_point = np.matmul(self.T.transformation, test)
+        print(self.punto) # imprimo el punto que me sale
+        #print(self.punto)
+        #print(self.pose_real)
+
+        # publicacion del punto
+        tolist = self.punto.tolist()
         tolist.pop(3)
 
         PoseMsg = Pose()
@@ -79,7 +93,8 @@ class Loc:
 
         self.pos_T_pub.publish(PoseMsg)
 
-        self.punto += self.new_point
+        # actualizacion de variables
+        self.punto_ant = self.punto
         self.nube_ant = self.nube
 
     def draw_registration_result(self):
